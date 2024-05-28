35c6a83e967aee086b8d01647a3b550269242e91
dmitriyn <d.nikitin.rf@gmail.com>
2024-05-28 16:11:55 +0500

    upd aythtest2

--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -32,23 +32,25 @@ class AuthTest {
     }
 
     @Test
-    @DisplayName("Should get error message if login with not registered user")
-    void shouldGetErrorIfNotRegisteredUser() {
-        var notRegisteredUser = getUser("active");
-        $("[data-test-id=login] input").setValue(notRegisteredUser.getLogin());
-        $("[data-test-id=password] input").setValue(notRegisteredUser.getPassword());
-        $("button.button").click();
-        $("[data-test-id='error-notification']. notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
-    }
-
-    @Test
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
         $("[data-test-id=login] input").setValue(blockedUser.getLogin());
         $("[data-test-id=password] input").setValue(blockedUser.getPassword());
         $("button.button").click();
-        $("[data-test-id='error-notification']. notification__content").shouldHave(Condition.text("Ошибка! Пользователь заблокирован"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
+        //$("[data-test-id='error-notification']. notification__content").shouldHave(Condition.text("Ошибка! Пользователь заблокирован"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
+        $("[class='notification__content']").shouldHave(Condition.text("Ошибка! Пользователь заблокирован"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        $("[data-test-id=login] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id=password] input").setValue(notRegisteredUser.getPassword());
+        $("button.button").click();
+        //$("[data-test-id='error-notification']. notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
+        $("[class='notification__content']").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
     }
 
     @Test
@@ -59,7 +61,8 @@ class AuthTest {
         $("[data-test-id=login] input").setValue(wrongLogin);
         $("[data-test-id=password] input").setValue(registeredUser.getPassword());
         $("button.button").click();
-        $("[data-test-id='error-notification']. notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
+        //$("[data-test-id='error-notification']. notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
+        $("[class='notification__content']").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
     }
 
     @Test
@@ -70,6 +73,7 @@ class AuthTest {
         $("[data-test-id=login] input").setValue(registeredUser.getLogin());
         $("[data-test-id=password] input").setValue(wrongPassword);
         $("button.button").click();
-        $("[data-test-id='error-notification']. notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
+        //$("[data-test-id='error-notification']. notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
+        $("[class='notification__content']").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10)).shouldBe(Condition.visible);
     }
 }
